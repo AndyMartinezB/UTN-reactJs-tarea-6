545e54b39c5b7c6c33c8e013559139f177a4409b
Andy Martinez Bologna <andres.bologna@mi.unc.edu.ar>
2026-01-02 12:13:15 -0300

    feat: implemento filtro y estilos. Reorganizo componentes

---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import './Home.css'
+import Usuarios from './Usuarios.jsx'
+
+function Home() {
+
+  return (
+    <>
+      <header>Tarea N°6: API REST en react</header>
+      <Usuarios />
+      <footer>Andrés Martínez Bologna, 2026\</footer>
+    </>
+  )
+}
+
+export default Home
--- src/components/Usuarios.jsx
+++ src/components/Usuarios.jsx
@@ -1,9 +1,12 @@
 import { useState, useEffect } from 'react'
+import './Usuarios.css'
+
 const Usuarios = () => {
 
     const [usuarios, setUsuarios] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+    const [busqueda, setBusqueda] = useState("");
 
     useEffect(() => {
         const fetchUsuarios = async () => {
@@ -20,21 +23,41 @@ const Usuarios = () => {
         };
         fetchUsuarios();
     }, []);
+    const usuariosFiltrados = usuarios.filter((usuario) =>
+    usuario.name.includes(busqueda)
+    );
+
     if (loading) return <div className= "spinning">Cargando usuarios...</div>;
     if (error) return <div className = "error-box">Se ha producido un error: {error.message}</div>;
     if (usuarios.length === 0) return <div className="lista-usuarios-vacia">No hay usuarios disponibles.</div>;
+    //if (busqueda.length > 0)
+   
     return (
     <>
     <div>
-        <h1>Usuarios</h1>
+        <h2>Usuarios</h2>
         <p>A continuación se listan los usuarios del sitio:</p>
     </div>
-    <ul>
-        {usuarios.map((usuario) => (
-            <li key={usuario.id}>
-                {usuario.name} - {usuario.email}
-            </li>
-        ))}
+    <div className="filtro-usuarios">
+        <input className="input-filtro"
+            type="text"
+            placeholder="Filtrar usuarios..."
+            value={busqueda}
+            onChange={(e) => setBusqueda(e.target.value)}  
+        />
+
+    </div>
+    <ul className="lista-usuarios">
+
+        {usuariosFiltrados.length === 0 ? (
+            <li>No se encuentran usuarios con "{busqueda}".</li>
+        ) : (
+            usuariosFiltrados.map((usuario) => (
+                <li key={usuario.id}>
+                    {usuario.name} - {usuario.email}
+                </li>
+            ))
+        )}
     </ul>
     </>
     );
